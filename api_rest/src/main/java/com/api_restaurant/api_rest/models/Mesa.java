35167521baf3814d4cc9f1ddd5092e51e5a2e80a
MartinMontagner <118780598+MartinMontagner@users.noreply.github.com>
2024-08-29 19:07:53 -0300

    Add files via upload

---
+++ api_rest/src/main/java/com/api_restaurant/api_rest/models/Mesa.java
@@ -0,0 +1,9 @@
+package com.api_restaurant.api_rest.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="mesa")
+public class Mesa {
+}
---
+++ api_rest/src/main/java/com/api_restaurant/api_rest/models/Waiter.java
@@ -0,0 +1,10 @@
+package com.api_restaurant.api_rest.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="mesero")
+
+public class Waiter {
+}
